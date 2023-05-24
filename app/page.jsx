569cbff226b0354bddcdec38bd66f74128acf2d0
Rahul-joshi10 <joshirahul329@gmail.com>
2023-05-24 11:00:13 +0530

    create-prompt-page

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 
 function Home() {
-  return (
-    <section className="w-full flex-center flex-col">
-      <h1 className="head_text text-center">
-        Discover & Share
-        <br className="max-md:hidden" />
-        <span className="orange_gradient text-center"> AI-Powered Prompt</span>
-      </h1>
-      <p className="desc text-center">
-        Promptopia a an open-source AI prompting tool for modern world to
-        discover, create and share creative prompts
-      </p>
-    </section>
-  );
+    return (
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">
+                Discover & Share
+                <br className="max-md:hidden" />
+                <span className="orange_gradient text-center">
+                    {' '}
+                    AI-Powered Prompt
+                </span>
+            </h1>
+            <p className="desc text-center">
+                Promptopia a an open-source AI prompting tool for modern world
+                to discover, create and share creative prompts
+            </p>
+        </section>
+    );
 }
 
 export default Home;
